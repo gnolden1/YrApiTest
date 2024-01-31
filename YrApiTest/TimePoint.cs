644ca4100db29e62aa827f30f4ec60ec79180190
gnolden1 <didrik.larsen@gmail.com>
2024-01-31 20:45:51 +0100

    Add project files.

---
+++ YrApiTest/TimePoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YrApiTest {
+    internal class TimePoint {
+        public DateTimeOffset time { get; set; }
+        public double air_pressure_at_sea_level { get; set; }
+        public double air_temperature { get; set; }
+        public double cloud_area_fraction { get; set; }
+        public double relative_humidity { get; set; }
+        public double wind_from_direction { get; set; }
+        public double wind_speed { get; set; }
+    }
+}
---
+++ YrApiTest/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YrApiTest {
+    internal class WeatherForecast {
+        public DateTimeOffset updated_at { get; set; }
+        public double[] coordinates { get; set; }
+        public List<TimePoint> timeseries { get; set; }
+    }
+}
